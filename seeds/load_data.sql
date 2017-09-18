a0e20f7a15114e65f6e935826a8a0e5d4114b7af
Breyana Scales <breyanaj@gmail.com>
2017-09-18 12:56:55 -0700

    Add seed data and script

---
+++ seeds/load_data.sql
@@ -0,0 +1,7 @@
+\c simple_bookstore
+
+COPY books(id, title, img_url, price, in_stock, isbn, publisher) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/books.csv' DELIMITERS ',' CSV;
+COPY authors(id, first_name, last_name) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/authors.csv' DELIMITERS ',' CSV;
+COPY genres(id, name) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/genres.csv' DELIMITERS ',' CSV;
+COPY authors_books(author_id, book_id) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/authors_books.csv' DELIMITERS ',' CSV;
+COPY genres_books(genre_id, book_id) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/genres_books.csv' DELIMITERS ',' CSV;
