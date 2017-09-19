58aaa347e62f6420f1ea67ce90dfde2334edb078
Breyana Scales <breyanaj@gmail.com>
2017-09-19 16:07:11 -0700

    Implement search view and route

--- src/server/routes/books.js
+++ src/server/routes/books.js
@@ -1,28 +1,28 @@
 const router = require('express').Router()
 const books = require('../../models/db/books')
 
-router.get('/:id', (request, response) => {
-  const id = request.params.id
-  books.getOneBook(id)
-    .then(book => {
-      response.render('books/book', { book })
-    })
-})
-
 router.get('/search', (request, response) => {
-  const searchTerm = request.body.query
+  const searchTerm = request.query.search
+    .toLowerCase()
     .replace(/^ */, '%')
     .replace(/ *$/, '%')
     .replace(/ +/g, '%')
+  let offset = 0
   if (request.query.start) {
-    let offset = request.query.start
-  } else {
-    let offset = 0
+    offset = request.query.start
   }
   books.searchForBooks(searchTerm, offset)
     .then(books => {
       response.render('books/search', { books })
     })
+})
+
+router.get('/:id', (request, response) => {
+  const id = request.params.id
+  books.getOneBook(id)
+  .then(book => {
+    response.render('books/book', { book })
+  })
 })
 
 module.exports = router
