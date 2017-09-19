1ef6decce80c70d4ba627f5cc62d1eb5fcbc5fd0
Breyana Scales <breyanaj@gmail.com>
2017-09-19 14:10:10 -0700

    Add search route

--- src/server/routes/books.js
+++ src/server/routes/books.js
@@ -9,4 +9,20 @@ router.get('/:id', (request, response) => {
     })
 })
 
+router.get('/search', (request, response) => {
+  const searchTerm = request.body.query
+    .replace(/^ */, '%')
+    .replace(/ *$/, '%')
+    .replace(/ +/g, '%')
+  if (request.query.start) {
+    let offset = request.query.start
+  } else {
+    let offset = 0
+  }
+  books.searchForBooks(searchTerm, offset)
+    .then(books => {
+      response.render('books/search', { books })
+    })
+})
+
 module.exports = router
