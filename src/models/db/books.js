2a930831d38b24155e4129b34c7265b8d7e31d87
Breyana Scales <breyanaj@gmail.com>
2017-09-19 11:30:41 -0700

    Add searchForBooks query

--- src/models/db/books.js
+++ src/models/db/books.js
@@ -24,8 +24,25 @@ const getOneBook = id => {
     .catch(error => console.error(error))
 }
 
+const searchForBooks = searchTerm => {
+  return db.query(`
+    SELECT title, price, img_url, first_name, last_name FROM books
+    JOIN authors_books ON books.id = authors_books.book_id
+    JOIN authors ON authors.id = authors_books.author_id
+    JOIN genres_books ON books.id = genres_books.book_id
+    JOIN genres ON genres.id = genres_books.genre_id
+    WHERE LOWER(title) LIKE $1
+    OR LOWER(first_name) LIKE $1
+    OR LOWER(last_name) LIKE $1
+    OR LOWER(name) LIKE $1
+    GROUP BY title, price, img_url, first_name, last_name;
+    `, [searchTerm])
+    .catch(error => console.error(error))
+}
+
 module.exports = {
   getAllBooks,
   getAllBookImages,
-  getOneBook
+  getOneBook,
+  searchForBooks
  }
