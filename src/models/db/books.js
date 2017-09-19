14e634184e571c3321ef6beec8387cc4baf73923
Breyana Scales <breyanaj@gmail.com>
2017-09-18 17:29:21 -0700

    Add getOneBook query function

--- src/models/db/books.js
+++ src/models/db/books.js
@@ -1,8 +1,22 @@
 const db = require('./db')
 
-const getAllBooks = function() {
+const getAllBooks = () => {
   return db.query('SELECT * FROM books')
-    .catch(error => console.error)
+    .catch(error => console.error(error))
+}
+
+const getOneBook = id => {
+  return db.query(`
+    SELECT title, price, img_url, in_stock, isbn, publisher, first_name, last_name, string_agg(genres.name, ', ') AS genres FROM books
+    JOIN authors_books ON books.id = authors_books.book_id
+    JOIN authors ON authors.id = authors_books.author_id
+    JOIN genres_books ON books.id = genres_books.book_id
+    JOIN genres ON genres.id = genres_books.genre_id
+    WHERE books.id = $1
+    GROUP BY title, price, img_url, in_stock, isbn, publisher, first_name, last_name
+    `, [id])
+    .then(result => result[0])
+    .catch(error => console.error(error))
 }
 
 module.exports = { getAllBooks }
