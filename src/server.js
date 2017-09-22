240a70b78b0c3bcba46c782666e8b262a80fe9d7
Breyana Scales <breyanaj@gmail.com>
2017-09-22 13:49:40 -0700

    Install method-override

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ const app = express();
 const routes = require('./server/routes')
 const middleware = require('./server/middleware')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(methodOverride('_method'))
 
 app.use(middleware)
 
