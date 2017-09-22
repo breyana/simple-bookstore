3da614673adce67a4d9660f16dd8b3126ecb7f49
Breyana Scales <breyanaj@gmail.com>
2017-09-22 13:36:57 -0700

    Edit getOneBook to return first names, last names, genres as arrays

--- src/models/db/books.js
+++ src/models/db/books.js
@@ -12,13 +12,18 @@ const getAllBookIdImages = () => {
 
 const getOneBook = id => {
   return db.query(`
-    SELECT books.id, title, price, img_url, in_stock, isbn, publisher, first_name, last_name, string_agg(genres.name, ', ') AS genres FROM books
+    SELECT books.id, title, price, img_url, in_stock, isbn, publisher,
+      array_agg(DISTINCT first_name) AS first_names,
+      array_agg(DISTINCT last_name) AS last_names,
+      array_agg(DISTINCT genres.name) AS genres
+    FROM books
     JOIN authors_books ON books.id = authors_books.book_id
     JOIN authors ON authors.id = authors_books.author_id
     JOIN genres_books ON books.id = genres_books.book_id
     JOIN genres ON genres.id = genres_books.genre_id
     WHERE books.id = $1
-    GROUP BY books.id, title, price, img_url, in_stock, isbn, publisher, first_name, last_name
+    GROUP BY books.id, title, price, img_url, in_stock, isbn, publisher
+    ORDER BY books.id
     `, [id])
     .then(result => result[0])
     .catch(error => console.error(error))
