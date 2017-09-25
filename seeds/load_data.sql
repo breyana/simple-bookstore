0753531fd405e4e7ab4f849d9c74d29ccd798bf5
Breyana Scales <breyanaj@gmail.com>
2017-09-25 11:37:49 -0700

    Change seeds to remove book ids

--- seeds/load_data.sql
+++ seeds/load_data.sql
@@ -1,6 +1,6 @@
 \c simple_bookstore
 
-COPY books(id, title, img_url, price, in_stock, isbn, publisher) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/books.csv' DELIMITERS ',' CSV;
+COPY books(title, img_url, price, in_stock, isbn, publisher) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/books.csv' DELIMITERS ',' CSV;
 COPY authors(first_name, last_name) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/authors.csv' DELIMITERS ',' CSV;
 COPY genres(name) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/genres.csv' DELIMITERS ',' CSV;
 COPY authors_books(author_id, book_id) FROM '/Users/breyana/Documents/Code/simple-bookstore/seeds/authors_books.csv' DELIMITERS ',' CSV;
