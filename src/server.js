0c041e385b141a93bd0079b7db9a0c8c2d187bd8
Breyana Scales <breyanaj@gmail.com>
2017-09-28 17:30:35 -0700

    Implement session database storage

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const middleware = require('./server/middleware')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const PgSession = require('connect-pg-simple')(session)
+const db = require('./models/db/db')
 require('dotenv').config()
 
 app.set('view engine', 'ejs')
@@ -16,9 +18,13 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
 
 app.use(session({
+  store: new PgSession({
+    pgPromise: db
+  }),
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
 }))
 
 app.use(middleware.setDefaultResponseLocals)
