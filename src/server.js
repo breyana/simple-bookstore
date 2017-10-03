156c1e1b20bcc3c508e105c4bff6bb55192862b4
Breyana Scales <breyanaj@gmail.com>
2017-10-03 16:39:18 -0700

    Add bodyparser.json()

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ app.set('views', __dirname + '/views')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.use(methodOverride('_method'))
 
